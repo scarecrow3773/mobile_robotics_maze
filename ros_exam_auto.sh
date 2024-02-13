6af2f788c12e19094abb01b1b3ddb2720529076b
Heiko Schoon <heiko.schoon@stud.hs-emden-leer.de>
2024-02-13 22:24:47 +0100

    docker compatible

--- ros_exam_auto.sh
+++ ros_exam_auto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd ~/ros2_ws/
-. install/local_setup.bash
-source /opt/ros/humble/setup.bash
+. install/local_setup.sh
+source /opt/ros/humble/setup.sh
 colcon build
-. install/local_setup.bash
-source /opt/ros/humble/setup.bash
+. install/local_setup.sh
+source /opt/ros/humble/setup.sh
 
 gnome-terminal -- ros2 launch mobile_robotics_hschoon mobile_robotics_hschoon.launch.py use_sim_time:=True
 
@@ -14,9 +14,9 @@ gnome-terminal -- ros2 launch nav2_bringup navigation_launch.py use_sim_time:=Tr
 gnome-terminal -- ros2 launch slam_toolbox online_async_launch.py use_sim_time:=True 
 
 gnome-terminal -- bash -c 'cd ~/ros2_ws/;
-. install/local_setup.bash;
+. install/local_setup.sh;
 ros2 launch explore_lite explore.launch.py; exec bash'
 
 gnome-terminal -- bash -c 'cd ~/ros2_ws/;
-. install/local_setup.bash;
+. install/local_setup.sh;
 ros2 run twist_mux twist_mux --ros-args --params-file ./src/mobile_robotics_hschoon/config/twist_mux.yaml -r cmd_vel_out:=simple_diff_drive_controller/cmd_vel_unstamped; exec bash'
